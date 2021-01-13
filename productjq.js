7ef42c48f2d589a1099742f5647e8deabeb837d7
Aliaranii <77276705+Aliaranii@users.noreply.github.com>
2021-01-13 05:23:56 +0100

    Update productjq.js

--- productjq.js
+++ productjq.js
@@ -2,13 +2,18 @@
 
 $(document).ready(function () {
 
-    $('#mycartnumber').text(i + " " + "items");
+    //Standard Wareninhalt von 0 beim Laden der Seite
+    standardcart();
 
+    //Erhöhen des Warencounters im Cart durch Produktbuttons
     $('#productbtn1, #productbtn2, #productbtn3, #productbtn4, #productbtn5, #productbtn6, #productbtn7, #productbtn8, #productbtn9').click(function() {
         updatecart();
     });
 });
 
+function standardcart() {
+    $('#mycartnumber').text(i + " " + "items");
+}
 
 function updatecart() {
     $('#mycartnumber').text((i = i + 1) + ' ' + 'items');
