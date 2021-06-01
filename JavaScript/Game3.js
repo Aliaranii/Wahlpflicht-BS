eda4fc6bad58bdf9e14ae0e1fbf7cebe1bd093b7
Viktoria Schrudde <viktoria.schrudde@neuland-bfi.de>
2021-06-01 19:45:47 +0200

    Ziel definiert Es wird jetzt angezeigt welcher Spieler dran ist

--- JavaScript/Game3.js
+++ JavaScript/Game3.js
@@ -8,18 +8,47 @@ let player1Turn;
 
 function startGame() {
     if (player1Turn === true) {
-      //  console.log("Spieler 1 ist dran");
+        console.log("Spieler 1 ist dran");
         movePlayer1();
-        randomActionCard();
+        if (positionPlayer1 === 54) {
+            console.log("ziel erreicht Spieler 1");
+            finishedGame();
+        } else {
+            randomActionCard();
+        }
         return;
     }
     if (player1Turn === false) {
-      //  console.log("Spieler 2 ist dran");
+        console.log("Spieler 2 ist dran");
         movePlayer2();
-        randomActionCard();
+        if (positionPlayer2 === 54) {
+            console.log("ziel erreicht Spieler 2");
+            finishedGame();
+        } else {
+            randomActionCard();
+        }
         return;
     }
 }
+
+function finishedGame() {
+    console.log("ziel erreicht")
+    document.getElementById("cube").style.display = "none";
+    document.getElementById("start-game-again-field").style.display = "block";
+    document.getElementById("finishedField").style.display = "block";
+    if (positionPlayer1 === 54) {
+        document.getElementById("finishedBox").innerText = "Spieler 1 gewinnt.";
+    }
+    if (positionPlayer2 === 54) {
+        document.getElementById("finishedBox").innerText = "Spieler 2 gewinnt.";
+    }
+}
+
+function reloadGame() {
+    location.reload();
+    return false;
+}
+
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -31,25 +60,26 @@ function randomActionCard() {
             let randomActionCardValues = actionCard[getRandomInt(actionCard.length)];
             document.getElementById("actionCardText").innerText = randomActionCardValues.text;
             if (randomActionCardValues.cube === true) {
-              //  console.log("Würfel würfeln")
+                //  console.log("Würfel würfeln")
                 document.getElementById("action-card-cube-button").style.display = "block";
                 document.getElementById('action-card-placeholder-picture').style.display = "block";
             }
             if (randomActionCardValues.field !== 0) {
-                if (randomActionCardValues.field === 1){
+                if (randomActionCardValues.field === 1) {
                     if (player1Turn === true) {
                         /* Spieler2 */
-                        if (positionPlayer2 === 1) {
+                        if (positionPlayer2 !== 1) {
                             if (positionPlayer2 !== positionPlayer1) {
                                 document.getElementById("" + positionPlayer2 + "").innerHTML = "";
                                 document.getElementById("player2").style.display = "block";
-                            } else{
+                            } else {
                                 document.getElementById("" + positionPlayer2 + "").innerHTML = "";
                                 document.getElementById("" + positionPlayer2 + "").innerHTML = "<span id='player1'></span>";
                                 document.getElementById("player2").style.display = "block";
                             }
                         }
                         positionPlayer2 = randomActionCardValues.field;
+                       // console.log("Spieler2 " + positionPlayer2);
                     }
                     if (player1Turn === false) {
                         /* Spieler1 */
@@ -57,51 +87,35 @@ function randomActionCard() {
                             if (positionPlayer1 !== positionPlayer2) {
                                 document.getElementById("" + positionPlayer1 + "").innerHTML = "";
                                 document.getElementById("player1").style.display = "block";
-                            } else{
+                            } else {
                                 document.getElementById("" + positionPlayer1 + "").innerHTML = "";
                                 document.getElementById("" + positionPlayer1 + "").innerHTML = "<span id='player2'></span>";
                                 document.getElementById("player1").style.display = "block";
                             }
                         }
                         positionPlayer1 = randomActionCardValues.field;
+                        // console.log("Spieler1 " + positionPlayer2);
                     }
                 } else {
                     //console.log("feld wechseln")
                     if (player1Turn === true) {
                         /* Spieler2 */
 
-                           // console.log("Spieler2")
-                            if (positionPlayer2 === 1) {
-                                document.getElementById("player2").style.display = "none";
-                            }
-                            if (positionPlayer2 !== positionPlayer1) {
-                                document.getElementById("" + positionPlayer2 + "").innerHTML = "";
-                            } else {
-                                document.getElementById("" + positionPlayer2 + "").innerHTML = "";
-                                document.getElementById("" + positionPlayer2 + "").innerHTML = "<span id='player1'></span>";
-                            }
-                            positionPlayer2 = randomActionCardValues.field;
-                            document.getElementById("" + positionPlayer2 + "").innerHTML += "<span id='player2'></span>";
-                            // console.log(positionPlayer2);
+                        // console.log("Spieler2")
+                        removePLayer2()
+                        positionPlayer2 = randomActionCardValues.field;
+                        document.getElementById("" + positionPlayer2 + "").innerHTML += "<span id='player2'></span>";
+                        // console.log("Spieler2 " + positionPlayer2);
 
                     }
                     if (player1Turn === false) {
                         /* Spieler1 */
 
-                            //console.log("Spieler1")
-                            if (positionPlayer1 === 1) {
-                                document.getElementById("player1").style.display = "none";
-                            }
-
-                            if (positionPlayer1 !== positionPlayer2) {
-                                document.getElementById("" + positionPlayer1 + "").innerHTML = "";
-                            } else {
-                                document.getElementById("" + positionPlayer1 + "").innerHTML = "";
-                                document.getElementById("" + positionPlayer1 + "").innerHTML = "<span id='player2'></span>";
-                            }
-                            positionPlayer1 = randomActionCardValues.field;
-                            document.getElementById("" + positionPlayer1 + "").innerHTML += "<span id='player1'></span>";
-                           // console.log(positionPlayer1);
+                        //console.log("Spieler1")
+                        removePlayer1()
+                        positionPlayer1 = randomActionCardValues.field;
+                        document.getElementById("" + positionPlayer1 + "").innerHTML += "<span id='player1'></span>";
+                       //  console.log("Spiler1 " + positionPlayer1);
 
                     }
                 }
@@ -114,6 +128,11 @@ function randomActionCard() {
 function closeTextfield() {
     document.getElementById("actionCard").style.display = "none";
     resetActionCardCubePicture();
+}
+
+function closeFinishedField() {
+    location.reload();
+    return false;
 }
 
 /*
@@ -130,7 +149,6 @@ function setPlayers() {
    } else {
        document.getElementById("default-text-player").style.display = "block";
    }
-
 }
 */
 
@@ -138,29 +156,48 @@ function setPlayer() {
     positionPlayer1 = 1;
     positionPlayer2 = 1;
     player1Turn = true;
+
     for (let i = 1; i <= 2; i++) {
         document.getElementById("1").innerHTML += "<span id=player" + i + "></span>";
     }
     document.getElementById("number-of-players-control").style.display = "none";
-    document.getElementById("cube-button").style.display = "block";
-    document.getElementById('placeholder-picture').style.display = "block";
+    document.getElementById("cube").style.display = "block";
+
 }
 
 function movePlayer1() {
-    if (positionPlayer1 === 1) {
-        document.getElementById("player1").style.display = "none";
-    } else if (positionPlayer1 === positionPlayer2) {
-        document.getElementById("" + positionPlayer1 + "").innerHTML = "";
-        document.getElementById("" + positionPlayer1 + "").innerHTML = "<span id='player2'></span>";
-    } else if (positionPlayer1 !== 1) {
-        document.getElementById("" + positionPlayer1 + "").innerHTML = "";
-    }
+    removePlayer1()
+
     positionPlayer1 += result;
-    document.getElementById("" + positionPlayer1 + "").innerHTML += "<span id='player1'></span>"
+   // console.log(positionPlayer1)
+    if (positionPlayer1 >= 54) {
+       // console.log("Kann der mal im ziel ankommen");
+        positionPlayer1 = 54;
+        document.getElementById("" + positionPlayer1 + "").innerHTML += "<span id='player1'></span>";
+    } else {
+        document.getElementById("" + positionPlayer1 + "").innerHTML += "<span id='player1'></span>";
+    }
+
     player1Turn = false;
 }
 
 function movePlayer2() {
+    removePLayer2()
+
+    positionPlayer2 += result;
+    // console.log(positionPlayer2)
+    if (positionPlayer2 >= 54) {
+      //  console.log("Kann der mal im ziel ankommen");
+        positionPlayer2 = 54;
+        document.getElementById("" + positionPlayer2 + "").innerHTML += "<span id='player2'></span>";
+    } else {
+        document.getElementById("" + positionPlayer2 + "").innerHTML += "<span id='player2'></span>";
+    }
+
+    player1Turn = true;
+}
+
+function removePLayer2() {
     if (positionPlayer2 === 1) {
         document.getElementById("player2").style.display = "none";
     } else if (positionPlayer2 === positionPlayer1) {
@@ -169,10 +206,17 @@ function movePlayer2() {
     } else if (positionPlayer2 !== 1) {
         document.getElementById("" + positionPlayer2 + "").innerHTML = "";
     }
+}
 
-    positionPlayer2 += result;
-    document.getElementById("" + positionPlayer2 + "").innerHTML += "<span id='player2'></span>";
-    player1Turn = true;
+function removePlayer1() {
+    if (positionPlayer1 === 1) {
+        document.getElementById("player1").style.display = "none";
+    } else if (positionPlayer1 === positionPlayer2) {
+        document.getElementById("" + positionPlayer1 + "").innerHTML = "";
+        document.getElementById("" + positionPlayer1 + "").innerHTML = "<span id='player2'></span>";
+    } else if (positionPlayer1 !== 1) {
+        document.getElementById("" + positionPlayer1 + "").innerHTML = "";
+    }
 }
 
 function resetActionCardCubePicture() {
@@ -187,7 +231,6 @@ function changeActionCardCubePicture() {
             setTimeout(function timer() {
                 for (let i = 0; i < 6; i++) {
                     setTimeout(function timer() {
-                        /**randomPicture**/
                         let randomPicture = Math.floor(Math.random() * 6) + 1;
                         document.getElementById('action-card-placeholder-picture').src = pictures[randomPicture - 1];
                     }, i * 500);
@@ -197,7 +240,14 @@ function changeActionCardCubePicture() {
     })
 }
 
-/**Bilder Tauschen**/
+function sayWhichPlayersItIs() {
+    if (player1Turn === true) {
+        document.getElementById("saysWhichPlayerIsIt").innerText = "Spieler 1 ist dran.";
+    } else {
+        document.getElementById("saysWhichPlayerIsIt").innerText = "Spieler 2 ist dran.";
+    }
+}
+
 function changeCubePicture() {
 
     let numberOfPlayers = 2; //document.getElementById("number").value;
@@ -229,6 +279,7 @@ function changeCubePicture() {
         }, k / k * 6000);
     }
     startGame();
+    sayWhichPlayersItIs();
 }
 
 //id 1: start
